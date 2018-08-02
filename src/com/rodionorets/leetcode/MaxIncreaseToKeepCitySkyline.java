22406e2dcea01d643dd3e8cc39ad3e9c9e8c5ef6
Rodion Orets <rodiksb@gmail.com>
2018-08-02 14:50:37 +0300

    Max Increase To Keep City Skyline done: beats 98.76% of Java submissions, 7ms runtime

--- src/com/rodionorets/leetcode/MaxIncreaseToKeepCitySkyline.java
+++ src/com/rodionorets/leetcode/MaxIncreaseToKeepCitySkyline.java
@@ -6,40 +6,32 @@ import java.util.List;
 public class MaxIncreaseToKeepCitySkyline {
 
     public static int maxIncreaseKeepingSkyline(int[][] grid) {
-        List<Integer> skylineFromLeft = new ArrayList<>();
-        List<Integer> skylineFromTop = new ArrayList<>();
+        int gridLength = grid[0].length;
+        int[] skylineFromLeft = new int[gridLength];
+        int[] skylineFromTop = new int[gridLength];
 
-        int originGridSize = grid.length;
+        int originGridCellValue;
+        for (int i = 0; i < gridLength; i++) {
+            for (int j = 0; j < gridLength; j++) {
+                originGridCellValue = grid[i][j];
+                if (originGridCellValue > skylineFromTop[j]) {
+                    skylineFromTop[j] = originGridCellValue;
+                }
 
-        for (int i = 0; i < originGridSize; i++) {
-            skylineFromLeft.add(findMaxValue(grid[i]));
-            int[] column = new int[originGridSize];
-            for (int j = 0; j < originGridSize; j++) {
-                column[j] = grid[j][i];
+                if (originGridCellValue > skylineFromLeft[i]) {
+                    skylineFromLeft[i] = originGridCellValue;
+                }
             }
-            skylineFromTop.add(findMaxValue(column));
         }
 
         int heightDifference = 0;
-        for (int i = 0; i < originGridSize; i++) {
-            for (int j = 0 ; j < originGridSize; j++) {
-
+        for (int i = 0; i < gridLength; i++) {
+            for (int j = 0 ; j < gridLength; j++) {
+                originGridCellValue = grid[i][j];
+                heightDifference += Math.min(skylineFromTop[j], skylineFromLeft[i]) - originGridCellValue;
             }
         }
 
         return heightDifference;
     }
-
-    private static int findMaxValue(int[] arr) {
-        int max = arr[0];
-
-        for (int item : arr) {
-            if (item > max) {
-                max = item;
-            }
-        }
-
-        return max;
-    }
-
 }
