a2a06430fd9ad9fe29bd5a3970e4ba704e75871f
rodionorets <rodiksb@gmail.com>
2018-08-05 11:20:25 +0300

    Container With Most Water: beats 89.89% of Java submissions, 5ms runtime

---
+++ src/com/rodionorets/leetcode/ContainerWithMostWater.java
@@ -0,0 +1,22 @@
+package com.rodionorets.leetcode;
+
+public class ContainerWithMostWater {
+    public static int maxArea(int[] height) {
+        int length = height.length;
+        int indexFromBegin = 0;
+        int indexFromEnd = length - 1;
+
+        int area = 0;
+        while (indexFromBegin < indexFromEnd) {
+            if (height[indexFromBegin] < height[indexFromEnd]) {
+                area = Math.max(area, height[indexFromBegin] * (indexFromEnd - indexFromBegin));
+                indexFromBegin++;
+            } else {
+                area = Math.max(area, height[indexFromEnd] * (indexFromEnd - indexFromBegin));
+                indexFromEnd--;
+            }
+        }
+
+        return area;
+    }
+}
