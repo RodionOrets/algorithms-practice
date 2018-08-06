e53188c834c51ccd1b990d26bd19d34e6a36cc19
Rodion Orets <rodiksb@gmail.com>
2018-08-06 15:48:38 +0300

    Binary Tree Inorder Traversal: recursive solution done, 0ms runtime, beats 100% of java submissions

---
+++ src/com/rodionorets/leetcode/BinaryTreeInorderTraversal.java
@@ -0,0 +1,34 @@
+package com.rodionorets.leetcode;
+
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.List;
+
+public class BinaryTreeInorderTraversal {
+
+    public class TreeNode {
+        int val;
+        TreeNode left;
+        TreeNode right;
+
+        TreeNode(int x) {
+            val = x;
+        }
+    }
+
+    public List<Integer> inorderTraversalRecursive(TreeNode root) {
+        List<Integer> list = new ArrayList<>();
+
+        if (root == null) return list;
+
+        list.addAll(inorderTraversalRecursive(root.left));
+        list.add(root.val);
+        list.addAll(inorderTraversalRecursive(root.right));
+
+        return list;
+    }
+
+    public List<Integer> inorderTraversalIterative(TreeNode node) {
+        return null;
+    }
+}
