f4852432a8c0da52c77c088f6448063cbac42cda
Rodion Orets <rodiksb@gmail.com>
2018-08-06 16:22:59 +0300

    Search in a binary tree: done, 2ms runtime, beats 100% of Java submissions

---
+++ src/com/rodionorets/leetcode/SearchInABinarySearchTree.java
@@ -0,0 +1,25 @@
+package com.rodionorets.leetcode;
+
+public class SearchInABinarySearchTree {
+
+    public class TreeNode {
+        int val;
+        TreeNode left;
+        TreeNode right;
+        TreeNode(int x) { val = x; }
+    }
+
+    public TreeNode searchBST(TreeNode root, int val) {
+        if (root == null) {
+            return null;
+        }
+
+        if (val < root.val) {
+            return searchBST(root.left, val);
+        } else if (val > root.val) {
+            return searchBST(root.right, val);
+        } else {
+            return root;
+        }
+    }
+}
