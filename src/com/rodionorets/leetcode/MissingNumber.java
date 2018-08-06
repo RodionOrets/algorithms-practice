df76a6d90038203da3aa115de073e509083db833
Rodion Orets <rodiksb@gmail.com>
2018-08-06 12:26:55 +0300

    Missing Number: done, 1ms runtime, beats 49% java submissions

---
+++ src/com/rodionorets/leetcode/MissingNumber.java
@@ -0,0 +1,15 @@
+package com.rodionorets.leetcode;
+
+public class MissingNumber {
+    public int missingNumber(int[] nums) {
+        int length = nums.length;
+        double requiredSum = (double)length / 2 * (length + 1);
+
+        int actualSum = 0;
+        for (int num : nums) {
+            actualSum += num;
+        }
+
+        return (int)requiredSum - actualSum;
+    }
+}
