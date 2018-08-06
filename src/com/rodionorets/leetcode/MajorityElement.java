f998c86cca53caae4634ca09ca1ca3fce2345297
Rodion Orets <rodiksb@gmail.com>
2018-08-06 16:52:00 +0300

    Majority Element: done, 3ms runtime, beats 100% of Java submissions

---
+++ src/com/rodionorets/leetcode/MajorityElement.java
@@ -0,0 +1,44 @@
+package com.rodionorets.leetcode;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class MajorityElement {
+    public int majorityElement(int[] nums) {
+        // Beats 33.96% of Java submissions, 17ms runtime
+//        Map<Integer, Integer> numbersToAppearances = new HashMap<>();
+//        int majorityElement = nums[0];
+//        int majorityElementAppearancesCount = 0;
+//
+//        for (int num : nums) {
+//            if (numbersToAppearances.containsKey(num)) {
+//                int n = numbersToAppearances.get(num) + 1;
+//                numbersToAppearances.put(num, n + 1);
+//                if (n > majorityElementAppearancesCount) {
+//                    majorityElement = num;
+//                    majorityElementAppearancesCount = n;
+//                }
+//            } else {
+//                numbersToAppearances.put(num, 1);
+//            }
+//        }
+//
+//        return majorityElement;
+
+        int appearanceCount = 0;
+        int majorityElement = 0;
+
+        for(int num : nums){
+            if(majorityElement == num){
+                appearanceCount += 1;
+            } else if (appearanceCount == 0){
+                majorityElement = num;
+                appearanceCount = 1;
+            }else {
+                appearanceCount -= 1;
+            }
+        }
+
+        return majorityElement;
+    }
+}
