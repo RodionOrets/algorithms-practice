1865aaf525d871bee8c582a4b5155e869ce6deeb
Rodion Orets <rodiksb@gmail.com>
2018-08-06 15:12:33 +0300

    Reverse linked list: done, 0ms runtime, beats 100% of java submissions

---
+++ src/com/rodionorets/leetcode/ReverseLinkedList.java
@@ -0,0 +1,21 @@
+package com.rodionorets.leetcode;
+
+public class ReverseLinkedList {
+
+    public class ListNode {
+        int val;
+        ListNode next;
+        ListNode(int x) { val = x; }
+    }
+
+    public ListNode reverseList(ListNode head) {
+        if (head == null || head.next == null) {
+            return head;
+        } else {
+            ListNode newHead = reverseList(head.next);
+            head.next.next = head;
+            head.next = null;
+            return newHead;
+        }
+    }
+}
