2822f0abdbaf82fea6a241202b864d14ad7956c9
Rodion Orets <rodiksb@gmail.com>
2018-08-07 17:03:50 +0300

    Convert Sorted Array to BST: done, 0ms runtime, beats 100% of Java submissions

---
+++ src/com/rodionorets/leetcode/ConvertSortedArrayToBST.java
@@ -0,0 +1,30 @@
+package com.rodionorets.leetcode;
+
+public class ConvertSortedArrayToBST {
+
+    public class TreeNode {
+        int val;
+        TreeNode left;
+        TreeNode right;
+        TreeNode(int x) { val = x; }
+    }
+
+    public TreeNode sortedArrayToBST(int[] nums) {
+        return sortedArrayToBST(nums, 0, nums.length - 1);
+    }
+
+    private TreeNode sortedArrayToBST(int[] nums, int from, int to) {
+        if (from > to) {
+            return null;
+        }
+
+        int middle = (from + to) / 2;
+
+        TreeNode root = new TreeNode(nums[middle]);
+
+        root.left = sortedArrayToBST(nums, from, middle - 1);
+        root.right = sortedArrayToBST(nums, middle + 1, to);
+
+        return root;
+    }
+}
