a466ed52c070eeed24ae4ac0002d1716b591321b
Rodion Orets <rodiksb@gmail.com>
2018-08-07 17:33:21 +0300

    Convert Sorted List to BST: done, 1ms runtime, beats 100% of Java submissions

---
+++ src/com/rodionorets/leetcode/ConvertSortedListToBinarySearchTree.java
@@ -0,0 +1,43 @@
+package com.rodionorets.leetcode;
+
+public class ConvertSortedListToBinarySearchTree {
+
+    public class TreeNode {
+        int val;
+        TreeNode left;
+        TreeNode right;
+        TreeNode(int x) { val = x; }
+    }
+
+    public class ListNode {
+        int val;
+        ListNode next;
+        ListNode(int x) { val = x; }
+    }
+
+
+    public TreeNode sortedListToBST(ListNode head) {
+        if (head==null){
+            return null;
+        }
+
+        return sortedListToBST(head, null);
+    }
+
+    private TreeNode sortedListToBST(ListNode head, ListNode tail){
+        ListNode slowRunner = head;
+        ListNode fastRunner = head;
+
+        if(head == tail) return null;
+
+        while(fastRunner != tail && fastRunner.next != tail){
+            fastRunner = fastRunner.next.next;
+            slowRunner = slowRunner.next;
+        }
+
+        TreeNode newHead = new TreeNode(slowRunner.val);
+        newHead.left = sortedListToBST(head, slowRunner);
+        newHead.right = sortedListToBST(slowRunner.next, tail);
+        return newHead;
+    }
+}
