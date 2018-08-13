607bd23da934ce907545baa1ad2addb0de1ecadf
Rodion Orets <rodiksb@gmail.com>
2018-08-13 12:44:42 +0300

    Maximum binary tree: description add

--- src/com/rodionorets/leetcode/trees/MaximumBinaryTree.java
+++ src/com/rodionorets/leetcode/trees/MaximumBinaryTree.java
@@ -1,5 +1,15 @@
 package com.rodionorets.leetcode.trees;
 
+/*
+Given an integer array with no duplicates. A maximum tree building on this array is defined as follow:
+
+1. The root is the maximum number in the array.
+2. The left subtree is the maximum tree constructed from left part subarray divided by the maximum number.
+3. The right subtree is the maximum tree constructed from right part subarray divided by the maximum number.
+
+Construct the maximum tree by the given array and output the root node of this tree.
+* */
+
 public class MaximumBinaryTree {
 
     public TreeNode constructMaximumBinaryTree(int[] nums) {
