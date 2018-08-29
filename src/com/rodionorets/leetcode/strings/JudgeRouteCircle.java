d0990233c2814589286df6420e1cb58029c42844
Rodion Orets <rodiksb@gmail.com>
2018-08-29 16:56:55 +0300

    Judge route circle: done, 9ms runtime, beats 82.99% of Java submissions

--- src/com/rodionorets/leetcode/strings/JudgeRouteCircle.java
+++ src/com/rodionorets/leetcode/strings/JudgeRouteCircle.java
@@ -5,19 +5,13 @@ import java.util.Map;
 
 public class JudgeRouteCircle {
     public boolean judgeCircle(String moves) {
-        int verticalDistance = 0;
-        int horizontalDistance = 0;
+        int[] hashingArray = new int[26];
 
         for (int i = 0; i < moves.length(); i++) {
             char move = moves.charAt(i);
-
-            if (move == 'U') verticalDistance++;
-            if (move == 'D') verticalDistance--;
-
-            if (move == 'R') horizontalDistance++;
-            if (move == 'L') horizontalDistance--;
+            hashingArray[move - 'A'] += 1;
         }
 
-        return verticalDistance == 0 && horizontalDistance == 0;
+        return hashingArray['U' - 'A'] == hashingArray['D' - 'A'] && hashingArray['L' - 'A'] == hashingArray['R' - 'A'];
     }
 }
