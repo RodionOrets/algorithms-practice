61891ff4e0d710552ad6529e08e2411edec5d1f3
Rodion Orets <rodiksb@gmail.com>
2018-08-29 15:33:23 +0300

    Single number: done, 0ms runtime, beats 100% of Java submissions

--- src/com/rodionorets/leetcode/numbers/SingleNumber.java
+++ src/com/rodionorets/leetcode/numbers/SingleNumber.java
@@ -5,15 +5,12 @@ import java.util.List;
 
 public class SingleNumber {
     public int singleNumber(int[] nums) {
-        List<Integer> numbers = new ArrayList<>();
+        int singleNumber = 0;
+
         for (int i = 0; i < nums.length; i++) {
-            if (!numbers.contains(nums[i])) {
-                numbers.add(nums[i]);
-            } else {
-                numbers.remove((Integer)nums[i]);
-            }
+            singleNumber = singleNumber ^ nums[i];
         }
 
-        return numbers.get(0);
+        return singleNumber;
     }
 }
