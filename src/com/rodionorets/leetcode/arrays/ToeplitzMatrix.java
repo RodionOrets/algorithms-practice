d54e4be79ba19c74ce926bcb0fa91ff0c3b15357
Rodion Orets <rodiksb@gmail.com>
2018-09-10 14:38:55 +0300

    Toeplitz matrix: done, 16ms runtime, beats 79.32% of Java submissions

--- src/com/rodionorets/leetcode/arrays/ToeplitzMatrix.java
+++ src/com/rodionorets/leetcode/arrays/ToeplitzMatrix.java
@@ -2,38 +2,12 @@ package com.rodionorets.leetcode.arrays;
 
 public class ToeplitzMatrix {
     public boolean isToeplitzMatrix(int[][] matrix) {
-        boolean[][] visited = new boolean[matrix.length][matrix[0].length];
-        boolean[][] isToeplitz = new boolean[matrix.length][matrix[0].length];
-
-        for (int i = 0; i < matrix.length; i++) {
-            for (int j = 0; j < matrix[i].length; j++) {
-                if (!isDiagonalContainsSameElements(matrix, visited, isToeplitz, i, j)) {
+        for (int i = 0; i < matrix.length - 1; i++) {
+            for (int j = 0; j < matrix[i].length - 1; j++) {
+                if (matrix[i][j] != matrix[i + 1][j + 1]) {
                     return false;
                 }
             }
-        }
-
-        return true;
-    }
-
-    private boolean isDiagonalContainsSameElements(int[][] matrix, boolean[][] visited, boolean[][] isToeplitz, int r, int c) {
-        if (visited[r][c]) {
-            return isToeplitz[r][c];
-        }
-
-        int rowIterator = r;
-        int columnIterator = c;
-
-        int element = matrix[r][c];
-        while (rowIterator < matrix.length && columnIterator < matrix[0].length) {
-            visited[rowIterator][columnIterator] = true;
-            isToeplitz[rowIterator][columnIterator] = matrix[rowIterator][columnIterator] == element;
-            if (!isToeplitz[rowIterator][columnIterator]) {
-                return false;
-            }
-
-            rowIterator++;
-            columnIterator++;
         }
 
         return true;
