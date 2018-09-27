f4e6065f6d1fbdeda9d77dd11cd7b9821e2204ae
Rodion Orets <rodiksb@gmail.com>
2018-09-27 14:52:54 +0300

    Shortest unsorted continuous subarray: done, 19ms runtime, beats 63.72% of Java submissions

---
+++ src/com/rodionorets/leetcode/arrays/ShortestUnsortedContinuousSubarray.java
@@ -0,0 +1,29 @@
+package com.rodionorets.leetcode.arrays;
+
+public class ShortestUnsortedContinuousSubarray {
+    public int findUnsortedSubarray(int[] nums) {
+        int length = nums.length;
+
+        int maxFromBegin = nums[0];
+        int minFromEnd = nums[length - 1];
+
+        int unsortedSubarrayBegin = -1;
+        int unsortedSubarrayEnd = -2;
+
+        int subarrayBegin;
+        int subarrayEnd;
+
+        for (int i = 1; i < length; i++) {
+            subarrayBegin = i;
+            subarrayEnd = length - i - 1;
+
+            maxFromBegin = Math.max(maxFromBegin, nums[subarrayBegin]);
+            minFromEnd = Math.min(minFromEnd, nums[subarrayEnd]);
+
+            if (nums[i] < maxFromBegin) unsortedSubarrayEnd = subarrayBegin;
+            if (nums[length - i - 1] > minFromEnd) unsortedSubarrayBegin = subarrayEnd;
+        }
+
+        return unsortedSubarrayEnd - unsortedSubarrayBegin + 1;
+    }
+}
