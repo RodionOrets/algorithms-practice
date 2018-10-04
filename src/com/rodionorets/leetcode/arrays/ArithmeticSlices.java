aebf48d836e40c5d81f41166621099d5f2b9f484
Rodion Orets <rodiksb@gmail.com>
2018-10-04 12:30:25 +0300

    Arithmetic slices: done, 1ms runtime, beats 100.00% of Java submissions - REFACTOR to O(n)

--- src/com/rodionorets/leetcode/arrays/ArithmeticSlices.java
+++ src/com/rodionorets/leetcode/arrays/ArithmeticSlices.java
@@ -3,13 +3,12 @@ package com.rodionorets.leetcode.arrays;
 public class ArithmeticSlices {
     public int numberOfArithmeticSlices(int[] A) {
         int numberOfArithmeticSlices = 0;
+        int[] slicesCache = new int[A.length];
 
-        for (int i = 0; i < A.length - 2; i++) {
-            int diff = A[i + 1] - A[i];
-            int runner = i + 2;
-            while (runner < A.length && A[runner] - A[runner - 1] == diff) {
-                numberOfArithmeticSlices += 1;
-                runner++;
+        for (int i = 2; i < A.length; i++) {
+            if (A[i] - A[i - 1] == A[i - 1] - A[i - 2]) {
+                slicesCache[i] = slicesCache[i - 1] + 1;
+                numberOfArithmeticSlices += slicesCache[i];
             }
         }
 
