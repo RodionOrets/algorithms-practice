978703a86d5254c836e287b55dbcedb9b129a492
Rodion Orets <rodiksb@gmail.com>
2018-10-05 11:24:07 +0300

    Max increase to keep city skyline: done, 7ms runtime, beats 99.53% of Java submissions - Revisit, refactor, description add

--- src/com/rodionorets/leetcode/arrays/MaxIncreaseToKeepCitySkyline.java
+++ src/com/rodionorets/leetcode/arrays/MaxIncreaseToKeepCitySkyline.java
@@ -1,34 +1,33 @@
 package com.rodionorets.leetcode.arrays;
 
-import java.util.ArrayList;
-import java.util.List;
+/*
+In a 2 dimensional array grid, each value grid[i][j] represents the height of a building located there.
+We are allowed to increase the height of any number of buildings, by any amount (the amounts can be different for different buildings).
+Height 0 is considered to be a building as well.
+At the end, the "skyline" when viewed from all four directions of the grid, i.e. top, bottom, left, and right, must be the same as the skyline of the original grid.
+A city's skyline is the outer contour of the rectangles formed by all the buildings when viewed from a distance.
+What is the maximum total sum that the height of the buildings can be increased?
+* */
 
 public class MaxIncreaseToKeepCitySkyline {
+    public int maxIncreaseKeepingSkyline(int[][] grid) {
+        int gridLength = grid.length;
 
-    public static int maxIncreaseKeepingSkyline(int[][] grid) {
-        int gridLength = grid[0].length;
-        int[] skylineFromLeft = new int[gridLength];
         int[] skylineFromTop = new int[gridLength];
+        int[] skylineFromLeft = new int[gridLength];
 
-        int originGridCellValue;
         for (int i = 0; i < gridLength; i++) {
             for (int j = 0; j < gridLength; j++) {
-                originGridCellValue = grid[i][j];
-                if (originGridCellValue > skylineFromTop[j]) {
-                    skylineFromTop[j] = originGridCellValue;
-                }
-
-                if (originGridCellValue > skylineFromLeft[i]) {
-                    skylineFromLeft[i] = originGridCellValue;
-                }
+                skylineFromTop[j] = Math.max(skylineFromTop[j], grid[i][j]);
+                skylineFromLeft[i] = Math.max(skylineFromLeft[i], grid[i][j]);
             }
         }
 
         int heightDifference = 0;
+
         for (int i = 0; i < gridLength; i++) {
-            for (int j = 0 ; j < gridLength; j++) {
-                originGridCellValue = grid[i][j];
-                heightDifference += Math.min(skylineFromTop[j], skylineFromLeft[i]) - originGridCellValue;
+            for (int j = 0; j < gridLength; j++) {
+                heightDifference += Math.min(skylineFromTop[j], skylineFromLeft[i]) - grid[i][j];
             }
         }
 
