82fdf4c39bfdd4a34c74f91cc8ad9724fd9e79b2
Rodion Orets <rodiksb@gmail.com>
2018-10-08 13:20:56 +0300

    Self dividing numbers: done, 4ms runtime, beats 89.72% of Java submissions

--- src/com/rodionorets/leetcode/numbers/SelfDividingNumbers.java
+++ src/com/rodionorets/leetcode/numbers/SelfDividingNumbers.java
@@ -3,24 +3,47 @@ package com.rodionorets.leetcode.numbers;
 import java.util.ArrayList;
 import java.util.List;
 
+/*
+A self-dividing number is a number that is divisible by every digit it contains.
+For example, 128 is a self-dividing number because 128 % 1 == 0, 128 % 2 == 0, and 128 % 8 == 0.
+Also, a self-dividing number is not allowed to contain the digit zero.
+Given a lower and upper number bound, output a list of every possible self dividing number, including the bounds if possible.
+
+Example 1:
+Input:
+left = 1, right = 22
+Output: [1, 2, 3, 4, 5, 6, 7, 8, 9, 11, 12, 15, 22]
+
+Note:
+ - The boundaries of each input argument are 1 <= left <= right <= 10000.
+* */
+
 public class SelfDividingNumbers {
     public List<Integer> selfDividingNumbers(int left, int right) {
         List<Integer> selfDividingNumbers = new ArrayList<>();
 
         for (int number = left; number <= right; number++) {
-            int tempNumber = number;
-            boolean divisibleByDigit = true;
-            while (tempNumber > 1) {
-                int digit = tempNumber % 10;
-                divisibleByDigit = divisibleByDigit && (digit != 0) && (number % digit == 0);
-                tempNumber = tempNumber / 10;
-            }
-
-            if (divisibleByDigit) {
+            if (isDivisibleByOwnDigits(number)) {
                 selfDividingNumbers.add(number);
             }
         }
 
         return selfDividingNumbers;
+    }
+
+    private boolean isDivisibleByOwnDigits(int number) {
+        int tempNumber = number;
+        while (tempNumber > 1) {
+            if (!isDivisibleByDigit(number, tempNumber % 10)) {
+                return false;
+            }
+            tempNumber /= 10;
+        }
+
+        return true;
+    }
+
+    private boolean isDivisibleByDigit(int number, int digit) {
+        return digit != 0 && number % digit == 0;
     }
 }
