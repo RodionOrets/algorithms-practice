31d90b17e49eaf444c956accafe6d006836cd559
Rodion Orets <rodiksb@gmail.com>
2018-10-08 14:52:21 +0300

    Container With Most Water: done, 5ms runtime, beats 96.01% of Java submissions

--- src/com/rodionorets/leetcode/arrays/ContainerWithMostWater.java
+++ src/com/rodionorets/leetcode/arrays/ContainerWithMostWater.java
@@ -1,22 +1,31 @@
 package com.rodionorets.leetcode.arrays;
 
-public class ContainerWithMostWater {
-    public static int maxArea(int[] height) {
-        int length = height.length;
-        int indexFromBegin = 0;
-        int indexFromEnd = length - 1;
+/*
+Given n non-negative integers a1, a2, ..., an , where each represents a point at coordinate (i, ai). n vertical lines are drawn such that the two endpoints of line i is at (i, ai) and (i, 0).
+Find two lines, which together with x-axis forms a container, such that the container contains the most water.
+Note: You may not slant the container and n is at least 2.
+* */
 
-        int area = 0;
-        while (indexFromBegin < indexFromEnd) {
-            if (height[indexFromBegin] < height[indexFromEnd]) {
-                area = Math.max(area, height[indexFromBegin] * (indexFromEnd - indexFromBegin));
-                indexFromBegin++;
+public class ContainerWithMostWater {
+    public int maxArea(int[] heights) {
+        int maxArea = 0;
+
+        int left = 0;
+        int right = heights.length - 1;
+        while (left < right) {
+            int distance = right - left;
+            int leftHeight = heights[left];
+            int rightHeight = heights[right];
+            int area = distance * Math.min(leftHeight, rightHeight);
+            maxArea = Math.max(maxArea, area);
+
+            if (leftHeight < rightHeight) {
+                left++;
             } else {
-                area = Math.max(area, height[indexFromEnd] * (indexFromEnd - indexFromBegin));
-                indexFromEnd--;
+                right--;
             }
         }
 
-        return area;
+        return maxArea;
     }
 }
