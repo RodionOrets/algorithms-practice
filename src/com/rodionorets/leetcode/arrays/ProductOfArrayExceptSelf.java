2a67afe35c59b6b118d335b44963d7212280f84f
Rodion Orets <rodiksb@gmail.com>
2018-11-01 08:00:44 +0200

    Product of array except self: done, 1ms runtime, beats 100.00% of Java submissions

--- src/com/rodionorets/leetcode/arrays/ProductOfArrayExceptSelf.java
+++ src/com/rodionorets/leetcode/arrays/ProductOfArrayExceptSelf.java
@@ -1,4 +1,25 @@
 package com.rodionorets.leetcode.arrays;
 
 public class ProductOfArrayExceptSelf {
+    public int[] productExceptSelf(int[] nums) {
+        int[] productExceptSelf = new int[nums.length];
+
+        if (nums.length == 0) return productExceptSelf;
+
+        int sum = 1;
+
+        for (int i = 0; i < nums.length; i++) {
+            productExceptSelf[i] = sum;
+            sum *= nums[i];
+        }
+
+        sum = 1;
+
+        for (int i = productExceptSelf.length - 1; i >= 0; i--) {
+            productExceptSelf[i] *= sum;
+            sum *= nums[i];
+        }
+
+        return productExceptSelf;
+    }
 }
