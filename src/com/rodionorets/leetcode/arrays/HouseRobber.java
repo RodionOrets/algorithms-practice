6ac935c21b16e683710e8917309fa05468eb4b3d
Rodion Orets <rodiksb@gmail.com>
2019-02-01 18:54:01 +0200

    House Robber: done, 3ms, beats 99.81% of Java submissions

--- src/com/rodionorets/leetcode/arrays/HouseRobber.java
+++ src/com/rodionorets/leetcode/arrays/HouseRobber.java
@@ -4,15 +4,14 @@ public class HouseRobber {
     public int rob(int[] nums) {
         if (nums.length == 0) return 0;
 
-        int[] robbed = new int[nums.length + 1];
-
-        robbed[0] = 0;
-        robbed[1] = nums[0];
-
+        int prev = 0;
+        int current = nums[0];
         for (int i = 1; i < nums.length; i++) {
-            robbed[i + 1] = Math.max(robbed[i], robbed[i - 1] + nums[i]);
+            int next = Math.max(current, prev + nums[i]);
+            prev = current;
+            current = next;
         }
 
-        return robbed[nums.length];
+        return current;
     }
 }
