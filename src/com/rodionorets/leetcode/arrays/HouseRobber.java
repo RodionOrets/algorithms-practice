d795ca790b821c0bafa1e61c9a189fd9407bb93d
Rodion Orets <rodiksb@gmail.com>
2019-02-01 18:06:51 +0200

    House Robber: Time Limit Exceeded, N/Ams, beats N/A% of Java submissions

--- src/com/rodionorets/leetcode/arrays/HouseRobber.java
+++ src/com/rodionorets/leetcode/arrays/HouseRobber.java
@@ -2,11 +2,14 @@ package com.rodionorets.leetcode.arrays;
 
 public class HouseRobber {
     public int rob(int[] nums) {
-        return rob(nums, 0);
+        return rob(nums, 0, new int[nums.length]);
     }
 
-    private int rob(int[] nums, int house) {
+    private int rob(int[] nums, int house, int robbed[]) {
         if (house >= nums.length) return 0;
-        return Math.max(rob(nums, house + 1), nums[house] + rob(nums, house + 2));
+        if (robbed[house] > 0) return robbed[house];
+        int rob = Math.max(rob(nums, house + 1, robbed), nums[house] + rob(nums, house + 2, robbed));
+        robbed[house] = rob;
+        return rob;
     }
 }
