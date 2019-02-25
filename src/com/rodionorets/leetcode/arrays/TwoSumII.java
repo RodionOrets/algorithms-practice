cd31ac87c928a7ab28abf030843d92fb63168542
Rodion Orets <rodiksb@gmail.com>
2019-02-25 20:06:53 +0200

    Two Sum II - Input array is sorted: done, 0ms runtime, beats 100.00% of Java submissions

--- src/com/rodionorets/leetcode/arrays/TwoSumII.java
+++ src/com/rodionorets/leetcode/arrays/TwoSumII.java
@@ -2,29 +2,19 @@ package com.rodionorets.leetcode.arrays;
 
 public class TwoSumII {
     public int[] twoSum(int[] nums, int target) {
-        int[] indices = new int[2];
-
-        for (int i = 0; i < nums.length; i++) {
-            int expectedComplement = target - nums[i];
-            int complementIndex = search(nums, i + 1, nums.length - 1, expectedComplement);
-            if (complementIndex != -1) {
-                indices[0] = i + 1;
-                indices[1] = complementIndex + 1;
-                break;
+        int low = 0;
+        int high = nums.length - 1;
+        while (low < high) {
+            int sumCandidate = nums[low] + nums[high];
+            if (sumCandidate == target) {
+                return new int[] {low + 1, high + 1};
+            } else if (sumCandidate > target) {
+                high--;
+            } else if (sumCandidate < target) {
+                low++;
             }
         }
 
-        return indices;
-    }
-
-    private int search(int[] nums, int from, int to, int target) {
-        while (from <= to) {
-            int middle = (to + from) / 2;
-            if (nums[middle] == target) return middle;
-            else if (target < nums[middle]) to = middle - 1;
-            else if (target > nums[middle]) from = middle + 1;
-        }
-
-        return -1;
+        return new int[] {-1, -1};
     }
 }
