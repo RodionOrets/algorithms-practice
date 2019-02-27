30471206fa62253a72073bb365dc303a59e66970
Rodion Orets <rodiksb@gmail.com>
2019-02-27 19:37:16 +0200

    2 Keys Keyboard: TODO add

--- src/com/rodionorets/leetcode/dynamicprogramming/TwoKeysKeyboard.java
+++ src/com/rodionorets/leetcode/dynamicprogramming/TwoKeysKeyboard.java
@@ -1,6 +1,8 @@
 package com.rodionorets.leetcode.dynamicprogramming;
 
 public class TwoKeysKeyboard {
+
+    // TODO (1): Revisit with dynamic programming. Probably DP isn't a good choice for this problem, but on LeetCode it's in DP problems :P
     public int minSteps(int n) {
         int count = 1;
 
