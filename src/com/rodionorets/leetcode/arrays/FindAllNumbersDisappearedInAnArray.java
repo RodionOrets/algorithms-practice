662d2f712a803dd6058fd3b0bc2423fc5ee3914d
Rodion Orets <rodiksb@gmail.com>
2020-02-19 23:33:17 +0200

    Find All Numbers Disappeared in an Array: done, 5ms runtime, beats 89.03% of submissions

--- src/com/rodionorets/leetcode/arrays/FindAllNumbersDisappearedInAnArray.java
+++ src/com/rodionorets/leetcode/arrays/FindAllNumbersDisappearedInAnArray.java
@@ -2,14 +2,15 @@ package com.rodionorets.leetcode.dynamicprogramming;
 
 class FindAllNumbersDisappearedInAnArray {
     public List<Integer> findDisappearedNumbers(int[] nums) {
-        int[] flaggedNums = new int[nums.length];
         for (int i = 0; i < nums.length; i++) {
-            flaggedNums[nums[i] - 1] = -1;
+            int positionToFlag = Math.abs(nums[i]) - 1;
+            nums[positionToFlag] = 
+                nums[positionToFlag] > 0 ? -nums[positionToFlag] : nums[positionToFlag];
         }
         
         List<Integer> disappearedNumbers = new ArrayList<>();
-        for (int i = 0; i < flaggedNums.length; i++) {
-            if (flaggedNums[i] != -1) {
+        for (int i = 0; i < nums.length; i++) {
+            if (nums[i] > 0) {
                 disappearedNumbers.add(i + 1);
             }
         }
