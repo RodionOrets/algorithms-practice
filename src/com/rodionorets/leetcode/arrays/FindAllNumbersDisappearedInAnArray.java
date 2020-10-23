b28000154f83aaee26f5e345d6d3fc3598543268
Rodion Orets <rodiksb@gmail.com>
2020-10-23 22:12:41 +0300

    FindAllNumbersDisappearedInArray: 5ms, beats 82.49%

--- src/com/rodionorets/leetcode/arrays/FindAllNumbersDisappearedInAnArray.java
+++ src/com/rodionorets/leetcode/arrays/FindAllNumbersDisappearedInAnArray.java
@@ -4,8 +4,9 @@ class FindAllNumbersDisappearedInAnArray {
     public List<Integer> findDisappearedNumbers(int[] nums) {
         for (int i = 0; i < nums.length; i++) {
             int positionToFlag = Math.abs(nums[i]) - 1;
-            nums[positionToFlag] = 
-                nums[positionToFlag] > 0 ? -nums[positionToFlag] : nums[positionToFlag];
+            nums[positionToFlag] = nums[positionToFlag] > 0 
+                ? -nums[positionToFlag]
+                : nums[positionToFlag];
         }
         
         List<Integer> disappearedNumbers = new ArrayList<>();
