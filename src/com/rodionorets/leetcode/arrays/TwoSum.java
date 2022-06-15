51b5b035543c750541b5e9123dd81fb5ede80fb0
Rodion Orets <rodionorets@gmail.com>
2022-06-15 19:24:51 +0300

    Two Sum: revisit, 1ms, faster than 99.66% of submissions

--- src/com/rodionorets/leetcode/arrays/TwoSum.java
+++ src/com/rodionorets/leetcode/arrays/TwoSum.java
@@ -6,15 +6,14 @@ import java.util.Map;
 public class TwoSum {
     public int[] twoSum(int[] nums, int target) {
         Map<Integer, Integer> complements = new HashMap<>();
-
         for (int i = 0; i < nums.length; i++) {
-            int expectedComplement = target - nums[i];
-            if (complements.containsKey(expectedComplement)) {
-                return new int[] {complements.get(expectedComplement), i};
+            int complement = target - nums[i];
+            if (complements.containsKey(complement)) {
+                return new int[] { complements.get(complement), i };
+            } else {
+                complements.put(nums[i], i);
             }
-            complements.put(nums[i], i);
         }
-
-        return new int[] {-1, -1};
+        return new int[] { -1, -1 };
     }
 }
