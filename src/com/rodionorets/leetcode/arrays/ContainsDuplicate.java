e3d17cb929e2d007c20cc5251a09cf5ef2bd3192
Rodion Orets <rodionorets@gmail.com>
2022-06-15 19:31:39 +0300

    Contains Duplicate: revisit, 8ms, faster than 82.65% of submissions

--- src/com/rodionorets/leetcode/arrays/ContainsDuplicate.java
+++ src/com/rodionorets/leetcode/arrays/ContainsDuplicate.java
@@ -5,12 +5,12 @@ import java.util.Set;
 
 public class ContainsDuplicate {
     public boolean containsDuplicate(int[] nums) {
-        Set<Integer> unique = new HashSet<>();
-
+        Set<Integer> distinctNums = new HashSet<>();
         for (int i = 0; i < nums.length; i++) {
-            if (!unique.add(nums[i])) return true;
+            if(!distinctNums.add(nums[i])) {
+                return true;
+            }
         }
-
         return false;
     }
 }
