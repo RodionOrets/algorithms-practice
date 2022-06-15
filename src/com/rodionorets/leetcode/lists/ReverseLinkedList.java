a85345918c3d948b689bf73f73998e0ca495f1b1
Rodion Orets <rodionorets@gmail.com>
2022-06-15 20:07:28 +0300

    Reverse linked list: revisit, 0ms, faster than 100.00% of submissions

--- src/com/rodionorets/leetcode/lists/ReverseLinkedList.java
+++ src/com/rodionorets/leetcode/lists/ReverseLinkedList.java
@@ -4,11 +4,19 @@ public class ReverseLinkedList {
     public ListNode reverseList(ListNode head) {
         if (head == null || head.next == null) {
             return head;
-        } else {
-            ListNode newHead = reverseList(head.next);
-            head.next.next = head;
-            head.next = null;
-            return newHead;
         }
+
+        ListNode previous = null;
+        ListNode current = head;
+        ListNode next;
+
+        while(current != null) {
+            next = current.next;
+            current.next = previous;
+            previous = current;
+            current = next;
+        }
+
+        return previous;
     }
 }
