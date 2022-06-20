3d40e7c43a2937830ec0e92ad82384dd8c5298f0
Rodion Orets <rodionorets@gmail.com>
2022-06-21 00:06:04 +0300

    Find Minimum In Rotated Sorted Array: done, 0ms, faster than 100.00% of submissions

---
+++ src/com/rodionorets/leetcode/arrays/FindMinimumInRotatedSortedArray.java
@@ -0,0 +1,33 @@
+package com.rodionorets.leetcode.arrays;
+
+public class FindMinimumInRotatedSortedArray {
+
+    public int findMin(int[] nums) {
+        if (nums[0] <= nums[nums.length - 1]) {
+            return nums[0];
+        }
+
+        int left = 0;
+        int right = nums.length - 1;
+        while (left < right) {
+            int pivot = left + (right - left) / 2;
+
+            if (nums[pivot] > nums[pivot + 1]) {
+                return nums[pivot + 1];
+            }
+
+            if (nums[pivot] < nums[pivot - 1]) {
+                return nums[pivot];
+            }
+
+            if (nums[pivot] > nums[0]) {
+                left = pivot + 1;
+            } else {
+                right = pivot - 1;
+            }
+        }
+
+        return -1;
+    }
+
+}
