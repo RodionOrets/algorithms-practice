88c07fc3964b87152d48c1ca66e29262a3d45188
Rodion Orets <rodionorets@gmail.com>
2022-06-20 23:06:55 +0300

    Add LCA of BST (just for the daily commit)

---
+++ src/com/rodionorets/leetcode/trees/LowestCommonAncestorOfBinarySearchTree.java
@@ -0,0 +1,9 @@
+package com.rodionorets.leetcode.trees;
+
+public class LowestCommonAncestorOfBinarySearchTree {
+
+    public TreeNode lowestCommonAncestor(TreeNode root, TreeNode p, TreeNode q) {
+        return null;
+    }
+
+}
