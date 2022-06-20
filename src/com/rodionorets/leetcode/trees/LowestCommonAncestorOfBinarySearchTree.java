8b95ddfe7ab1551a01e2a1d960de63e2a36fa776
Rodion Orets <rodionorets@gmail.com>
2022-06-21 01:00:13 +0300

    Lowest Common Ancestor Of Binary Tree: done, 8ms, faster than 74.29% of submissions

--- src/com/rodionorets/leetcode/trees/LowestCommonAncestorOfBinarySearchTree.java
+++ src/com/rodionorets/leetcode/trees/LowestCommonAncestorOfBinarySearchTree.java
@@ -3,6 +3,22 @@ package com.rodionorets.leetcode.trees;
 public class LowestCommonAncestorOfBinarySearchTree {
 
     public TreeNode lowestCommonAncestor(TreeNode root, TreeNode p, TreeNode q) {
+        if (root == null) return null;
+
+        if (root.val == p.val || root.val == q.val) {
+            return root;
+        }
+
+        TreeNode lcaOnLeft = lowestCommonAncestor(root.left, p, q);
+        TreeNode lcaOnRight = lowestCommonAncestor(root.right, p, q);
+
+        if (lcaOnLeft != null && lcaOnRight != null) {
+            return root;
+        }
+
+        if (lcaOnLeft != null) return lcaOnLeft;
+        if (lcaOnRight != null) return lcaOnRight;
+
         return null;
     }
 
