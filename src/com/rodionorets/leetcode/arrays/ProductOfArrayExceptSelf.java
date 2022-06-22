5d924ebbe1dd375c3041de5e6637d03b10971e3f
Rodion Orets <rodionorets@gmail.com>
2022-06-22 23:32:03 +0300

    Products Of Array Except Self: revisit, 0ms, faster than 100.00% of submissions

--- src/com/rodionorets/leetcode/arrays/ProductOfArrayExceptSelf.java
+++ src/com/rodionorets/leetcode/arrays/ProductOfArrayExceptSelf.java
@@ -2,26 +2,26 @@ package com.rodionorets.leetcode.arrays;
 
 public class ProductOfArrayExceptSelf {
     public int[] productExceptSelf(int[] nums) {
-        int[] productsExceptSelf = new int[nums.length];
-        int[] productsToTheLeft = new int[nums.length];
-        int[] productsToTheRight = new int[nums.length];
-        
-        for (int i = 0; i < productsToTheLeft.length; i++) {
-            productsToTheLeft[i] = i == 0 
-                ? 1 
-                : productsToTheLeft[i - 1] * nums[i - 1];
+        int length = nums.length;
+
+        int[] productsExceptSelf = new int[length];
+        int[] productsFromLeftToRight = new int[length];
+        int[] productsFromRightToLeft = new int[length];
+
+        productsFromLeftToRight[0] = 1;
+        for (int i = 1; i < length; i++) {
+            productsFromLeftToRight[i] = productsFromLeftToRight[i - 1] * nums[i - 1];
         }
-        
-        for (int i = productsToTheRight.length - 1; i >= 0; i--) {
-            productsToTheRight[i] = i == productsToTheRight.length - 1 
-                ? 1 
-                : productsToTheRight[i + 1] * nums[i + 1];
+
+        productsFromRightToLeft[length - 1] = 1;
+        for (int i = length - 2; i >= 0; i--) {
+            productsFromRightToLeft[i] = productsFromRightToLeft[i + 1] * nums[i + 1];
         }
-        
-        for (int i = 0; i < productsExceptSelf.length; i++) {
-            productsExceptSelf[i] = productsToTheLeft[i] * productsToTheRight[i];
+
+        for (int i = 0; i < length; i++) {
+            productsExceptSelf[i] = productsFromLeftToRight[i] * productsFromRightToLeft[i];
         }
-        
+
         return productsExceptSelf;
     }
 }
