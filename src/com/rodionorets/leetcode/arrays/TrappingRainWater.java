0db4d10832f383786dc2efaaad9dfd7f832cc93f
Rodion Orets <rodionorets@gmail.com>
2022-06-25 00:12:06 +0300

    Traping Rain Water: revisit, 1ms runtime, beats 98.95% of Java submissions

--- src/com/rodionorets/leetcode/arrays/TrappingRainWater.java
+++ src/com/rodionorets/leetcode/arrays/TrappingRainWater.java
@@ -14,28 +14,30 @@ public class TrappingRainWater {
     public int trap(int[] heights) {
         if (heights.length <= 2) return 0;
         
-        int maxIndex = 0;
+        int maxHeightIndex = 0;
         for (int i = 0; i < heights.length; i++) {
-            if (heights[i] > heights[maxIndex]) maxIndex = i;
+            if (heights[i] > heights[maxHeightIndex]) {
+                maxHeightIndex = i;
+            }
         }
         
         int trappedWater = 0;
         
-        int leftMax = heights[0];
-        for (int i = 1; i < maxIndex; i++) {
-            if (heights[i] > leftMax) {
-                leftMax = heights[i];
+        int maxHeightToTheLeftOfTotalMaxHeight = heights[0];
+        for (int i = 1; i < maxHeightIndex; i++) {
+            if (heights[i] > maxHeightToTheLeftOfTotalMaxHeight) {
+                maxHeightToTheLeftOfTotalMaxHeight = heights[i];
             } else {
-                trappedWater += leftMax - heights[i];
+                trappedWater += maxHeightToTheLeftOfTotalMaxHeight - heights[i];
             }
         }
         
-        int rightMax = heights[heights.length - 1];
-        for (int i = heights.length - 2; i > maxIndex; i--) {
-            if (heights[i] > rightMax) {
-                rightMax = heights[i];
+        int maxHeightToTheRightOfTotalMaxHeight = heights[heights.length - 1];
+        for (int i = heights.length - 2; i > maxHeightIndex; i--) {
+            if (heights[i] > maxHeightToTheRightOfTotalMaxHeight) {
+                maxHeightToTheRightOfTotalMaxHeight = heights[i];
             } else {
-                trappedWater += rightMax - heights[i];
+                trappedWater += maxHeightToTheRightOfTotalMaxHeight - heights[i];
             }
         }
         
